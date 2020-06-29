1198016ff1c5a10ff606a8aaf941016fd4755b6a
Austin Oie <austinoie@gmail.com>
2020-06-29 16:38:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,7 @@ def list_dwarves(array)
   array.each_with_index { |item, index| 
   hash[item] = index
   }
+  puts array 
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
